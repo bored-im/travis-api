5694b91306eeb6ce8998fd25c29839cdc15407a1
Yuva <yuva@codemancers.com>
2018-09-08 11:22:04 +0530

    disable ssl loading

--- lib/travis/api/app.rb
+++ lib/travis/api/app.rb
@@ -139,7 +139,7 @@ module Travis::Api
           use Travis::Api::App::Middleware::OpenCensus
         end
 
-        use Rack::SSL if Endpoint.production?
+        # use Rack::SSL if Endpoint.production?
         use ActiveRecord::ConnectionAdapters::ConnectionManagement
         use ActiveRecord::QueryCache
 
